80cf93eed58a5d895bcb02371416d25f42e936ba
manish7511 <137864864+manish7511@users.noreply.github.com>
2023-11-24 07:35:55 +0530

    Add files via upload

---
+++ clearlist.py
@@ -0,0 +1,14 @@
+# GEEK = [6, 0, 4, 1]
+# GEEK.clear()
+# print(GEEK)
+
+
+ #  Clearing a Python List Using “*= 0”
+
+# list=[1,2,3,4,5]
+# list*=0
+# print(list)
+
+list=[3,4,5,6,7]
+list=[]
+print(list)
