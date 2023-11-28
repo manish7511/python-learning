2e612dfbbd62698b40c9b4b4bb069dd47eec1462
manish7511 <137864864+manish7511@users.noreply.github.com>
2023-11-28 10:37:29 +0530

    Add files via upload
    ARRAY

---
+++ SUM.py
@@ -0,0 +1,17 @@
+# Python Program to Find Sum of Array
+
+from array import *
+a=array('i',[1,2,3,4,5])
+sum=0
+for i in a:
+    sum=sum+i
+print("sum of array is",sum)
+
+
+from array import *
+def sumarr(a):
+    sum=0
+    for i in a:
+        sum=sum+i
+a=array('i',[1,2,3,4,5])
+print("sum of array is",sum)
